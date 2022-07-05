8fa9d8535aa5436820143bcad2ec20e08e2d5980
Mh Tusher <mh.developer.me@gmail.com>
2022-07-05 07:24:38 +0600

    reply migration correction

--- database/migrations/2022_07_05_004855_create_replies_table.php
+++ database/migrations/2022_07_05_004855_create_replies_table.php
@@ -17,8 +17,8 @@ class CreateRepliesTable extends Migration
             $table->id();
             $table->text('body');
             $table->integer('question_id')->unsigne();
-            $table->integer('user_id')->unsigne();
-            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
+            $table->integer('user_id');
+            // $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
             $table->timestamps();
         });
     }
