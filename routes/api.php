66612baa140fab9960a0023631f91edba2132960
Mh Tusher <mh.developer.me@gmail.com>
2022-08-10 03:39:28 +0600

    category api done

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,9 @@
 <?php
 
 use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\ReplyController;
 
 Route::apiResource('/question', QuestionController::class);
+Route::apiResource('/category', CategoryController::class);
+Route::apiResource('/question/{question}/reply', ReplyController::class);
